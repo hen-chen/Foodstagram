ba22eaf2d18f0a3c975cd334587b02a51dd17cfc
Henry Chen <henchen@seas.upenn.edu>
2021-12-04 18:59:11 -0500

    food button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => (
-  <>
-    <h1> WEB DEV </h1>
-  </>
-)
+const App = () => {
+  const [image, setImage] = useState(null)
+
+  return (
+    <>
+      <h1> Food! </h1>
+      <button className="btn" onClick={() => {
+        fetch('https://foodish-api.herokuapp.com/api/')
+          .then(res => res.json())
+          .then(({ image }) => {
+            setImage(image)
+          })
+      }}>
+        Load food image!
+      </button>
+      <img src={image} />
+    </>
+  )
+}
 export default App
